4ba77154b6a2f3d4abcc28302c7f4dca27669f86
James Muranga <jmured@gmail.com>
2014-07-16 23:19:44 +0300

    James spike out syncing commodities

--- app/src/main/java/org/clintonhealthaccess/lmis/app/remote/endpoints/Dhis2Endpoint.java
+++ app/src/main/java/org/clintonhealthaccess/lmis/app/remote/endpoints/Dhis2Endpoint.java
@@ -1,18 +1,31 @@
 package org.clintonhealthaccess.lmis.app.remote.endpoints;
 
 import org.clintonhealthaccess.lmis.app.LmisException;
+import org.clintonhealthaccess.lmis.app.models.CategoryCombo;
+import org.clintonhealthaccess.lmis.app.models.DataElement;
+import org.clintonhealthaccess.lmis.app.models.DataSet;
 
 import java.util.List;
 import java.util.Map;
 
 import retrofit.http.GET;
+import retrofit.http.Path;
 
 public interface Dhis2Endpoint {
 
     @GET("/api/systemSettings/reasons_for_order")
-    Map<String,List<String>> getReasonsForOrder();
+    Map<String, List<String>> getReasonsForOrder();
 
     @GET("/api/users")
     Object getUsers() throws LmisException;
+
+    @GET("/api/dataSets/{id}")
+    DataSet getDataSet(@Path("id") String id);
+
+    @GET("/api/dataElements/{id}")
+    DataElement getDataElement(@Path("id") String id);
+
+    @GET("/api/categoryCombos/{id}")
+    CategoryCombo getCategoryCombo(@Path("id") String id);
 }
   
